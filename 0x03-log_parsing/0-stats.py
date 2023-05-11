e061f0cfb16064f91f4e25648e13d4943bba252e
emilykamiti <emilykamiti@gmail.com>
2023-05-11 06:04:21 +0000

    parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
+"""
+module contains a script that reads stdin line by line and computes metrics
+"""
 import sys
+
 
 status_codes = {
     "200": 0,
@@ -8,19 +11,23 @@ status_codes = {
     "400": 0,
     "401": 0,
     "403": 0,
+    "404": 0,
     "405": 0,
-    "500": 0,
+    "500": 0
 }
 
 file_size = 0
 
+
 def print_metrics():
+    """prints of the logs"""
     print("File size: {}".format(file_size))
     for status in sorted(status_codes.keys()):
         if status_codes[status]:
             print("{}: {}".format(status, status_codes[status]))
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     count = 0
     try:
         for line in sys.stdin:
@@ -28,7 +35,7 @@ if __name__== "__main__":
                 elems = line.split()
                 file_size += int(elems[-1])
                 if elems[-2] in status_codes:
-                    status_codes[elems[-2]] =+ 1
+                    status_codes[elems[-2]] += 1
             except Exception:
                 pass
             if count == 9:
