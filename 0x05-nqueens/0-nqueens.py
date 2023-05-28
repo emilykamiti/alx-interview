f1f1f933abc16351402bfcba29881b0921f950e6
emilykamiti <emilykamiti@gmail.com>
2023-05-28 16:02:19 +0000

    lit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-"""solve the N queen problem
-"""
+#!/usr/bin/python
+"""Solves the N Queens puzzle for NxN chessboord"""
 
 import sys
 
 
 def board_set_up(N):
-    """Creates a balnk chess board, NxN"""
+    """
+    Sets up blank NxN chessboard"""
     matrix = []
-    for now in range(N):
+    for row in range(N):
         matrix_row = []
         for column in range(N):
             matrix_row.append(0)
@@ -17,79 +17,67 @@ def board_set_up(N):
 
 
 def print_solution(matrix):
+    """Prints the coordinates where there is a queen"""
 
-    """Prints the coordinates of the queens on the bpard"""
-    queens_cordinates = []
+    queens_coordinates = []
     for i, row in enumerate(matrix):
         for j, column in enumerate(row):
             if column == 1:
-               queen = []
-               queen.append(i)
-               queen.append(j)
-               queens_coordinates.append(queen)
+                queen = []
+                queen.append(i)
+                queen.append(j)
+                queens_coordinates.append(queen)
     print(queens_coordinates)
 
 
 def is_safe(matrix, new_row, new_column):
-
-    """Checks if a queen can be placed in the given position"""
-    # Checks row up to column
+    """Determines if the queen is safe to be put in a new row or colomn"""
+    # checks row up to column
     for i in range(new_column):
         if matrix[new_row][i]:
             return False
-
-    # Checks upper diagonal
+    # checks upper diagonal
     for i, j in zip(range(new_row, -1, -1),
                     range(new_column, -1, -1)):
-         if matrix[i][j]:
+        if matrix[i][j]:
             return False
-
-    N = len(matrix
-) 
-    #Checks lower diagonal
+    N = len(matrix)
+    # checks lower diagonal
     for i, j in zip(range(new_row, N, 1),
-                     range(new_column, -1, -1)):
-         if matrix[i][j]:
+                    range(new_column, -1, -1)):
+        if matrix[i][j]:
             return False
-
     return True
 
 
 def solve(matrix, new_column):
-
-    """Solves the N Queen Puzzle recursively"""
-
     N = len(matrix)
-
-    #All queens are placed
+    # base case: all queens are placed
     if new_column >= N:
         print_solution(matrix)
         return matrix
-
     for new_row in range(N):
         if is_safe(matrix, new_row, new_column):
             matrix[new_row][new_column] = 1
-            #Try to recursively solve the rest of the queens
+            # call to recursively try to solve rest of queens
             solve(matrix, new_column + 1)
-            #If it can't solve, reset to 0
+            # if can't solve with this position, re-set as 0
             matrix[new_row][new_column] = 0
     return None
 
-    if __name__ == "__main__":
-        if len(sys.argv) != 2:
-           print("Usage: nqueens N")
-           sys.exit(1)
 
-        N = sys.argv[1]
-
-        try:
-            N = int(N)
-        except Exception as e:
-            print("N must be a number")
-            sys.exit(1)
-
-        if N < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        matrix = board_set_up(N)
-        solve(matrix, 0)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    N = sys.argv[1]
+    try:
+        N = int(N)
+    except Exception as e:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+    matrix = board_set_up(N)
+    solve(matrix, 0)
