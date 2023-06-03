7f51d1be51318bb364193a74ace9e65e9e5d29cd
emilykamiti <emilykamiti@gmail.com>
2023-06-03 18:28:11 +0000

    mit

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+// Start Wars Movie Characters Script
+// The movie ID is passed as the first position argument.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(err)
+    } else if (response.statusCode === 200) {
+      const film = JSON.parse(body);
+      const characterPromises = film.characters.map(characterURL =>
+        new Promise((resolve, reject) => {
+          request.get(characterURL, (err, response, body) => {
+            if (err) {
+              reject(err);
+             } else if (response.statusCode === 200) {
+               const character = JSON.parse(body);
+               resolve(character.name);
+             } else {
+               reject(new Error('request failed with status code ${response.statusCode}'));
+               }
+        });
+      })
+    );
+
+// Wait for all promises to resolve, then print the character names
+
+       Promise.all(characterPromises)
+      .then(characterNames => {
+        characterNames.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    console.log(`Request failed with status code ${response.statusCode}`);
+  }
+});
