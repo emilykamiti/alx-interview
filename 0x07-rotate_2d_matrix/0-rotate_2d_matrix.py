8bd54d195a9eff88fcdbdcefcc4b2c72d617a1ed
emilykamiti <emilykamiti@gmail.com>
2023-06-07 12:35:00 +0000

    thru

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -31,9 +31,6 @@ def rotate_2d_matrix(matrix):
         ValueError: If the input is not a valid 2d matrix.
     """
 
-    if not matrix or not all(len(row) == len(matrix[0]) for row in matrix):
-        raise ValueError("Input is not a valid 2d matrix.")
-
     left, right = 0, len(matrix) - 1
 
     while left < right:
@@ -41,7 +38,7 @@ def rotate_2d_matrix(matrix):
             top, bottom = left, right
             # Save the value of the top left element in a temporary variable 'top_left'
             top_left = matrix[top][left + i]
-            # Move the bottom Left to the top left position
+            # Move the bottom left to the top left position
             matrix[top][left + i] = matrix[bottom - i][left]
             # Move bottom right element to bottom left position
             matrix[bottom - i][left] = matrix[bottom][right - i]
