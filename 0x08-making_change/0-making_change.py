a1aefcc93e21fc0422374c4ddd4ce990b2d918d0
emilykamiti <emilykamiti@gmail.com>
2023-06-12 19:09:20 +0000

    mit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,6 +2,11 @@
 
 
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
+    """
+
     if total <= 0:
         return 0
 
