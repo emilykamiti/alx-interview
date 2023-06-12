5ef60ac40fd0dd921fe7d87c0c3395f02a5e2136
emilykamiti <emilykamiti@gmail.com>
2023-06-12 17:38:50 +0000

    mit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            if min_coins[i - coin] + 1 <min_coins[i]:
+                min_coins[i] = min_coins[i - coin] + 1
+
+    if min_coins[total] == float('inf'):
+        return -1
+    else:
+        return min_coins[total]
