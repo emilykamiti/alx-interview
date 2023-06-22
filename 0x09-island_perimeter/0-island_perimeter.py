86bde019e94188bffdd5d397a74d46b27fd598ca
emilykamiti <emilykamiti@gmail.com>
2023-06-22 15:31:19 +0000

    mit

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,7 +14,7 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                # check adjacent cells
+                # cells that are adjacent
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
