687e0298e2dc70f7fc23c95b578d86b5a2c3fc6b
emilykamiti <emilykamiti@gmail.com>
2023-06-28 10:08:52 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,28 +3,27 @@
 
 
 def isWinner(x, nums):
-
     def is_prime(n):
         if n < 2:
             return False
         for i in range(2, int(n ** 0.5) + 1):
             if n % i == 0:
                 return False
-            return True
+        return True
 
-        if x <= 0 or not nums:
-            return None
+    if x <= 0 or not nums:
+        return None
 
-        wins = {'Maria': 0, 'Ben': 0}
+    wins = {'Maria': 0, 'Ben': 0}
 
-        for n in nums:
-            prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-            if prime_count % 2 == 0:
-                wins['Ben'] += 1
-            else:
-                wins['Maria'] += 1
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            wins['Ben'] += 1
+        else:
+            wins['Maria'] += 1
 
-    if wins['Maria'] > wins['Bens']:
+    if wins['Maria'] > wins['Ben']:
         return 'Maria'
     elif wins['Maria'] < wins['Ben']:
         return 'Ben'
