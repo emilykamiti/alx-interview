f54ffe4d2031e3cbcfee8a5a14e2783e9036b3d0
emilykamiti <emilykamiti@gmail.com>
2023-07-12 07:55:29 +0000

    mit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,5 @@
-#!/usr/bin/python3
-""" Determines the winner of a prime game."""
-
-
 def isWinner(x, nums):
-    """Returns the number of rounds to be played"""
-
     def is_prime(n):
-        """Checks if number is prime"""
-
         if n < 2:
             return False
         for i in range(2, int(n ** 0.5) + 1):
@@ -23,9 +15,9 @@ def isWinner(x, nums):
     for n in nums:
         prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
         if prime_count % 2 == 0:
-            wins['Ben'] += 1
-        else:
             wins['Maria'] += 1
+        else:
+            wins['Ben'] += 1
 
     if wins['Maria'] > wins['Ben']:
         return 'Maria'
@@ -33,3 +25,6 @@ def isWinner(x, nums):
         return 'Ben'
     else:
         return None
+
+result = isWinner(10000, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print("Winner:", result)
