ee2c6fb10176f0a4d6e47d89a9b5c6884832426f
emilykamiti <emilykamiti@gmail.com>
2023-07-13 21:50:30 +0000

    news

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,58 @@
 #!/usr/bin/python3
-"""The player that cannot make a move loses the game."""
+
+"""
+Prime Game Module: Defines function that determines the winner after a certain
+number of rounds of playing the Prime Game.
+"""
 
 
 def isWinner(x, nums):
     """
-    returns:
-        the name of the player that won the most rounds,
-        if both players play optimally,
-        or None, if the winner cannot be determined
+    Determines the winner after a certain number of rounds
+    of playing the Prime Game.
+
+    The Prime Game is a game where Maria and Ben take turns choosing a prime number
+    from a set of consecutive integers starting from 1 up to and including n.
+    The chosen prime number and its multiples are removed from the set.
+    The player who cannot make a move loses the game.
+
+    They play x rounds of the game, with n potentially different for each round.
+    Assuming Maria always goes first and both players play optimally,
+    this function determines the winner of each game.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): List of n for each round.
+
+    Returns:
+        str: The name of the player that won the most rounds.
+             If the winner cannot be determined, returns None.
     """
-    if not nums or x < 1:  # if nums is empty or x is less than 1
+    if not nums or x < 1:
         return None
-    n = max(nums)
-    sieve = [True for _ in range(max(n + 1, 2))]
-    for i in range(2, int(pow(n, 0.5)) + 1):
-        if not sieve[i]:
-            continue
-        for j in range(i*i, n + 1, i):
-            sieve[j] = False  # set the value to False (not prime)
 
-    sieve[0] = sieve[1] = False
-    count = 0  # initialize count to 0 (Maria's score)
-    for i in range(len(sieve)):
-        if sieve[i]:
-            count += 1
-        sieve[i] = count  # set the value to the count of primes
+    def isPrime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-    player1 = 0
-    for n in nums:  # loop through the nums list and count Maria's wins
-        player1 += sieve[n] % 2 == 1
-    if player1 * 2 == len(nums):
+    maria_wins = 0
+    for n in nums:
+        primes = [i for i in range(2, n + 1) if isPrime(i)]
+        if len(primes) % 2 == 1:
+            maria_wins += 1
+
+    if maria_wins * 2 == x:
         return None
-    if player1 * 2 > len(nums):
-        return "Maria"  # Maria wins
-    return "Beiin"  # Ben wins
+    elif maria_wins * 2 > x:
+        return "Maria"
+    else:
+        return "Ben"
+
+
+if __name__ == "__main__":
+    # Example usage
+    print(isWinner(5, [2, 5, 1, 4, 3]))
